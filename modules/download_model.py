f6a9b8de32a7c463a0ce14dafb1250850c525e4d
Justin Gerard Ricardo <justinericardo@gmail.com>
2024-01-12 01:53:36 +0800

    chore: Add script to download model files

---
+++ modules/download_model.py
@@ -0,0 +1,21 @@
+from transformers import AutoModel, AutoTokenizer
+
+
+def main():
+    # Model name to use for text summarization
+    model_name = "facebook/bart-large-cnn"
+    # model_name = "sshleifer/distilbart-cnn-12-6"
+
+    # If model files exist locally, load them from disk.
+    # Otherwise, download them from HuggingFace's model hub.
+    model = AutoModel.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    print("[INFO] Model files have been downloaded.")
+
+    # Save the model files locally if they haven't been saved yet.
+    model.save_pretrained(model_name)
+    tokenizer.save_pretrained(model_name)
+
+
+if __name__ == "__main__":
+    main()
