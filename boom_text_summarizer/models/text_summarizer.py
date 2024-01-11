c598f33b5da669fd915bcbdc9e4a8b1ffcd26ad1
Justin Gerard Ricardo <justinericardo@gmail.com>
2024-01-12 02:24:19 +0800

    Define method for text summarization

--- boom_text_summarizer/models/text_summarizer.py
+++ boom_text_summarizer/models/text_summarizer.py
@@ -13,3 +13,19 @@ class TextSummarizer(object):
         text = text.replace("\n", "")
         text = unidecode(text)
         return text
+
+    def summarize(self, input_text):
+        input_text = self.preprocess_text(input_text)
+        inputs = self.tokenizer(
+            input_text,
+            max_length=1024,
+            truncation=True,
+            return_tensors="pt",
+        )
+        summary_ids = self.model.generate(inputs["input_ids"])
+        summary = self.tokenizer.batch_decode(
+            summary_ids,
+            skip_special_tokens=True,
+            clean_up_tokenization_spaces=True,
+        )
+        return summary[0]
