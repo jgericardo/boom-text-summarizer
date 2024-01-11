c77be25a37022571698113c5e748cd89be2b34d6
Justin Gerard Ricardo <justinericardo@gmail.com>
2024-01-12 02:08:04 +0800

    Define text summarizer constructor

--- boom_text_summarizer/models/text_summarizer.py
+++ boom_text_summarizer/models/text_summarizer.py
@@ -4,4 +4,6 @@ from transformers import AutoTokenizer, BartForConditionalGeneration
 
 
 class TextSummarizer(object):
-    pass
+    def __init__(self, model_name="sshleifer/distilbart-cnn-12-6"):
+        self.model = BartForConditionalGeneration.from_pretrained(model_name)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
