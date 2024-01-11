bd10ab651493f4185b47719c14ccf2a4728f9095
Justin Gerard Ricardo <justinericardo@gmail.com>
2024-01-12 02:11:09 +0800

    Add helper method to preprocess text

--- boom_text_summarizer/models/text_summarizer.py
+++ boom_text_summarizer/models/text_summarizer.py
@@ -1,9 +1,15 @@
 """Text summarizer class"""
 import torch
 from transformers import AutoTokenizer, BartForConditionalGeneration
+from unidecode import unidecode
 
 
 class TextSummarizer(object):
     def __init__(self, model_name="sshleifer/distilbart-cnn-12-6"):
         self.model = BartForConditionalGeneration.from_pretrained(model_name)
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    def preprocess_text(self, text):
+        text = text.replace("\n", "")
+        text = unidecode(text)
+        return text
